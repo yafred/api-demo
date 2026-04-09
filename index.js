b9739a79a7e9113be97131c4234b8be3a6672811
YaFred <yafred@users.noreply.github.com>
2026-04-09 08:39:36 +0200

    cannot use github actions yet

--- index.js
+++ index.js
@@ -7944,7 +7944,7 @@ function isShadowRoot(node) {
 
 // and applies them to the HTMLElements such as popper and arrow
 
-function applyStyles$1(_ref) {
+function applyStyles(_ref) {
   var state = _ref.state;
   Object.keys(state.elements).forEach(function (name) {
     var style = state.styles[name] || {};
@@ -8016,11 +8016,11 @@ function effect$2(_ref2) {
 } // eslint-disable-next-line import/no-unused-modules
 
 
-var applyStyles = {
+var applyStyles$1 = {
   name: 'applyStyles',
   enabled: true,
   phase: 'write',
-  fn: applyStyles$1,
+  fn: applyStyles,
   effect: effect$2,
   requires: ['computeStyles']
 };
@@ -8267,7 +8267,7 @@ var toPaddingObject = function toPaddingObject(padding, state) {
   return mergePaddingObject(typeof padding !== 'number' ? padding : expandToHashMap(padding, basePlacements));
 };
 
-function arrow$1(_ref) {
+function arrow(_ref) {
   var _state$modifiersData$;
 
   var state = _ref.state,
@@ -8331,11 +8331,11 @@ function effect$1(_ref2) {
 } // eslint-disable-next-line import/no-unused-modules
 
 
-var arrow = {
+var arrow$1 = {
   name: 'arrow',
   enabled: true,
   phase: 'main',
-  fn: arrow$1,
+  fn: arrow,
   effect: effect$1,
   requires: ['popperOffsets'],
   requiresIfExists: ['preventOverflow']
@@ -8456,7 +8456,7 @@ function mapToStyles(_ref2) {
   return Object.assign({}, commonStyles, (_Object$assign2 = {}, _Object$assign2[sideY] = hasY ? y + "px" : '', _Object$assign2[sideX] = hasX ? x + "px" : '', _Object$assign2.transform = '', _Object$assign2));
 }
 
-function computeStyles$1(_ref5) {
+function computeStyles(_ref5) {
   var state = _ref5.state,
       options = _ref5.options;
   var _options$gpuAccelerat = options.gpuAcceleration,
@@ -8498,11 +8498,11 @@ function computeStyles$1(_ref5) {
 } // eslint-disable-next-line import/no-unused-modules
 
 
-var computeStyles = {
+var computeStyles$1 = {
   name: 'computeStyles',
   enabled: true,
   phase: 'beforeWrite',
-  fn: computeStyles$1,
+  fn: computeStyles,
   data: {}
 };
 
@@ -8930,7 +8930,7 @@ function getExpandedFallbackPlacements(placement) {
   return [getOppositeVariationPlacement(placement), oppositePlacement, getOppositeVariationPlacement(oppositePlacement)];
 }
 
-function flip$1(_ref) {
+function flip(_ref) {
   var state = _ref.state,
       options = _ref.options,
       name = _ref.name;
@@ -9050,11 +9050,11 @@ function flip$1(_ref) {
 } // eslint-disable-next-line import/no-unused-modules
 
 
-var flip = {
+var flip$1 = {
   name: 'flip',
   enabled: true,
   phase: 'main',
-  fn: flip$1,
+  fn: flip,
   requiresIfExists: ['offset'],
   data: {
     _skip: false
@@ -9083,7 +9083,7 @@ function isAnySideFullyClipped(overflow) {
   });
 }
 
-function hide$1(_ref) {
+function hide(_ref) {
   var state = _ref.state,
       name = _ref.name;
   var referenceRect = state.rects.reference;
@@ -9112,12 +9112,12 @@ function hide$1(_ref) {
 } // eslint-disable-next-line import/no-unused-modules
 
 
-var hide = {
+var hide$1 = {
   name: 'hide',
   enabled: true,
   phase: 'main',
   requiresIfExists: ['preventOverflow'],
-  fn: hide$1
+  fn: hide
 };
 
 function distanceAndSkiddingToXY(placement, rects, offset) {
@@ -9141,7 +9141,7 @@ function distanceAndSkiddingToXY(placement, rects, offset) {
   };
 }
 
-function offset$1(_ref2) {
+function offset(_ref2) {
   var state = _ref2.state,
       options = _ref2.options,
       name = _ref2.name;
@@ -9164,15 +9164,15 @@ function offset$1(_ref2) {
 } // eslint-disable-next-line import/no-unused-modules
 
 
-var offset = {
+var offset$1 = {
   name: 'offset',
   enabled: true,
   phase: 'main',
   requires: ['popperOffsets'],
-  fn: offset$1
+  fn: offset
 };
 
-function popperOffsets$1(_ref) {
+function popperOffsets(_ref) {
   var state = _ref.state,
       name = _ref.name;
   // Offsets are the actual position the popper needs to have to be
@@ -9187,11 +9187,11 @@ function popperOffsets$1(_ref) {
 } // eslint-disable-next-line import/no-unused-modules
 
 
-var popperOffsets = {
+var popperOffsets$1 = {
   name: 'popperOffsets',
   enabled: true,
   phase: 'read',
-  fn: popperOffsets$1,
+  fn: popperOffsets,
   data: {}
 };
 
@@ -9199,7 +9199,7 @@ function getAltAxis(axis) {
   return axis === 'x' ? 'y' : 'x';
 }
 
-function preventOverflow$1(_ref) {
+function preventOverflow(_ref) {
   var state = _ref.state,
       options = _ref.options,
       name = _ref.name;
@@ -9322,11 +9322,11 @@ function preventOverflow$1(_ref) {
 } // eslint-disable-next-line import/no-unused-modules
 
 
-var preventOverflow = {
+var preventOverflow$1 = {
   name: 'preventOverflow',
   enabled: true,
   phase: 'main',
-  fn: preventOverflow$1,
+  fn: preventOverflow,
   requiresIfExists: ['offset']
 };
 
@@ -9658,12 +9658,12 @@ function popperGenerator(generatorOptions) {
 }
 var createPopper$2 = /*#__PURE__*/popperGenerator(); // eslint-disable-next-line import/no-unused-modules
 
-var defaultModifiers$1 = [eventListeners, popperOffsets, computeStyles, applyStyles];
+var defaultModifiers$1 = [eventListeners, popperOffsets$1, computeStyles$1, applyStyles$1];
 var createPopper$1 = /*#__PURE__*/popperGenerator({
   defaultModifiers: defaultModifiers$1
 }); // eslint-disable-next-line import/no-unused-modules
 
-var defaultModifiers = [eventListeners, popperOffsets, computeStyles, applyStyles, offset, flip, preventOverflow, arrow, hide];
+var defaultModifiers = [eventListeners, popperOffsets$1, computeStyles$1, applyStyles$1, offset$1, flip$1, preventOverflow$1, arrow$1, hide$1];
 var createPopper = /*#__PURE__*/popperGenerator({
   defaultModifiers: defaultModifiers
 }); // eslint-disable-next-line import/no-unused-modules
@@ -9673,8 +9673,8 @@ var lib = /*#__PURE__*/Object.freeze({
     afterMain: afterMain,
     afterRead: afterRead,
     afterWrite: afterWrite,
-    applyStyles: applyStyles,
-    arrow: arrow,
+    applyStyles: applyStyles$1,
+    arrow: arrow$1,
     auto: auto,
     basePlacements: basePlacements,
     beforeMain: beforeMain,
@@ -9682,24 +9682,24 @@ var lib = /*#__PURE__*/Object.freeze({
     beforeWrite: beforeWrite,
     bottom: bottom,
     clippingParents: clippingParents,
-    computeStyles: computeStyles,
+    computeStyles: computeStyles$1,
     createPopper: createPopper,
     createPopperBase: createPopper$2,
     createPopperLite: createPopper$1,
     detectOverflow: detectOverflow,
     end: end,
     eventListeners: eventListeners,
-    flip: flip,
-    hide: hide,
+    flip: flip$1,
+    hide: hide$1,
     left: left,
     main: main$1,
     modifierPhases: modifierPhases,
-    offset: offset,
+    offset: offset$1,
     placements: placements,
     popper: popper,
     popperGenerator: popperGenerator,
-    popperOffsets: popperOffsets,
-    preventOverflow: preventOverflow,
+    popperOffsets: popperOffsets$1,
+    preventOverflow: preventOverflow$1,
     read: read,
     reference: reference,
     right: right,
